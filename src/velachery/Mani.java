a13e9b34fac2500fb116de5905e92489c1ebf7b1
Mani <Mani@192.168.2.131>
2019-10-24 09:44:42 +0530

    editable ghitup

--- src/velachery/Mani.java
+++ src/velachery/Mani.java
@@ -3,8 +3,11 @@ package velachery;
 public class Mani {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+   int a=5;
+   int b=5;
+		 int  c=a+b;
+		 System.out.println(c);
 	}
 
 }
